4b67e9b3c24f481948bfe8c91b03c4d8267a47a1
Brian Icaro D. Marques <bi.dm@hotmail.com>
2019-04-16 13:05:37 -0300

    set and verify speed

--- ia2.py
+++ ia2.py
@@ -8,6 +8,13 @@ def criaParticulas():
         particulas.append([uniform(min(
             dominio1+dominio2), max(dominio1+dominio2)), uniform(0, max(dominio1+dominio2))])
     print(particulas)
+    return(particulas)
+
+
+# def atribuiVelocidade(particulas):
+#     for i in range(3):
+#         velocidade[i] = (uniform(min(limiteVelocidade[0]), max(limiteVelocidade[1]))
+#         print(velocidade)
 
 
 # def verificaVelocidade():
@@ -23,11 +30,10 @@ def criaParticulas():
 #            particula[1] = -100
 #            particula.velocidade = 0
 
-
 dominio1 = [-100, 100]
 dominio2 = [-100, 100]
 limiteVelocidade = [-15, 15]
 criaParticulas()
 inte = int(input("Entre com o numero de interacoes:"))
 for i in range(inte):
-    print('algo')
+    print(limiteVelocidade[0], limiteVelocidade[1])
