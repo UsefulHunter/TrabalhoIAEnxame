702fa9cfd9e76e0e4a203a2cfa5ccf064a6c08b5
Brian Icaro D. Marques <bi.dm@hotmail.com>
2019-04-23 13:17:06 -0300

    Update ia2.py

--- ia2.py
+++ ia2.py
@@ -5,62 +5,73 @@ from math import pow
 
 
 class Particle:
-    def __init__(self):
-        self.x1 = 0
-        self.y2 = 0
+    def __init__(self, x, y, velocidade, pb):
+        self.x = 0
+        self.y = 0
+        self.velocidadex = 0
+        self.velocidadey = 0
         self.pb = [-100, 100]
-        self.velocidade = 0
 
 
-def criaParticulas():  # 1.Determina o número de partículas P da população
+def criaParticulas():
     n = int(input("Entre com o numero de particulas:"))
-    particulas[n] = []
-    for i in range(n):  # 2.Inicializa aleatoriamente a posição inicial(x) de cada partícula p de P
-        particulas[i] = Particle(
-            uniform(dominio1[0], dominio1[1]), uniform(dominio2[0], dominio2[1]))
+    particulas = []
+    velocidadex = (uniform(min(limiteVelocidade), max(limiteVelocidade)))
+    velocidadey = (uniform(min(limiteVelocidade), max(limiteVelocidade)))
+    for i in range(n):
+        particulas[i] = Particle(uniform(dominio1[0], dominio1[1]), uniform(
+            dominio2[0], dominio2[1]), velocidadex, velocidadey)
     print(particulas)
-    return(particulas, n)
+    return(particulas)
 
 
-def rodaFuncao(particulas, n):
-    melhorParticula = 0
-    for i in range(n):  # 4.(a). Calcula Aptidão fp = f(p)
-        f[i] = 0.5 + (pow(sin(sqrt(pow(particulas[i].x1, 2) + (pow(particulas[i].x2, 2))), 2)) -
-                  0.5)/pow((1 + 0.001 * (pow(particulas[i].x1, 2) + (pow(particulas[i].x2, 2))), 2))
-        if(f[i] > melhorParticula):
-            melhorParticula = f[i]
-            print(melhorParticula)
-    return melhorParticula
+def rodaFuncao(particulas):
+    aptidao = []
+    melhorAptidao = 0
+    for i in range(len(particulas)):
+        aptidao.append(0.5 + (pow(sin(sqrt(pow(particulas[i].x, 2) + (pow(particulas[i].y, 2))), 2)) -
+                              0.5)/pow((1 + 0.001 * (pow(particulas[i].x, 2) + (pow(particulas[i].y, 2))), 2)))
+        if(aptidao[i] > melhorAptidao):
+            melhorAptidao = aptidao[i]
+            print(melhorAptidao)
+    return melhorAptidao
 
 
-def atribuiVelocidade(particulas, n):
-    for i in range(n):  # 3.Atribua uma velocidade inivial (v) igual para todas as particulas
-        velocidade[i] = (uniform(min(limiteVelocidade[0]), max(limiteVelocidade[1]))
-         particulas[i].velocidade=velocidade[i]
-         print(velocidade[i])
+def atribuiVelocidade(particulas):
+    velocidade = []
+    for i in range(len(particulas)):
+        velocidade.append(
+            uniform(min(limiteVelocidade[0]), max(limiteVelocidade[1]))))
+        particulas[i].velocidade=velocidade[i]
+        print(velocidade[i])
     return particulas
 
-def atualizaVelocidade(particulas, n):
+
+def atualizaVelocidade(particulas):
     novaVelocidade=0
-    for i in range(n):
-        novaVelocidade=particulas[i].velocidade + (particulas[i].x1 * rand(0, 1) * (
-            particulas[i].pb[0] - particulas[i].x1)) + particulas[i].x2 * rand(0, 1)*(gb - particulas[i].x2)
+    for i in range(len(particulas)):
+        novaVelocidade=particulas[i].velocidade + (particulas[i].x * rand(0, 1) * (
+            particulas[i].pb[0] - particulas[i].x)) + particulas[i].y * rand(0, 1)*(gb - particulas[i].y)
         particulas[i].velocidade=novaVelocidade
         verificaVelocidade(particulas)
         print(novaVelocidade)
 
-# def verificaVelocidade():
-#     for particula in particulas:
-#        if particula.velocidade > limiteVelocidade[1]
-#             particula.velocidade = limiteVelocidade[1]
-#             particula.x1 = 100
-#             particula.x2 = 100
-#             particula.velocidade = 0
-#         else if particula.velocidade < limiteVelocidade[0]:
-#            particula.velocidade = limiteVelocidade[0]
-#            particula.x1 = -100
-#            particula.x2 = -100
-#            particula.velocidade = 0
+
+def atualizaPosicao(particulas):
+    novaPosicaox=-100
+    novaPosicaoy=100
+    for i in range(len(particulas)):
+        novaPosicaox=particulas[i].x + particulas[i].velocidade
+        novaPosicaoy=particulas[i].y + particulas[i].velocidade
+
+
+def aplicaPesoInercial(particulas):
+    for i in range(len(particulas)):
+        particulas[i].velocidade=rand(0.4, 0.9) * particulas[i].velocidade
+
+def calculoPonderacao(particulas):
+    for i in range(len(particulas)):
+        w=0.9 - inte*((0.9 - 0.4)/inte)
 
 dominio1=[-100, 100]
 dominio2=[-100, 100]
